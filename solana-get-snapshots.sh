25d64064ca9c00bf5956aa8baa6c36b396f33791
leonardo3dp <88781157+leonardo3dp@users.noreply.github.com>
2023-12-21 16:05:41 -0300

    Update solana-get-snapshots.sh

--- solana-get-snapshots.sh
+++ solana-get-snapshots.sh
@@ -2,7 +2,7 @@
 # Only when required, manually grab snapshots from Solana labs
 set -e
 
-sudo -u sol mkdir -p /home/sol/ledger
-cd /home/sol/ledger
+sudo -u sol mkdir -p /mt/ledger/validator-ledger
+cd /mt/ledger/validator-ledger
 sudo -u sol wget --trust-server-names http://145.40.67.83/snapshot.tar.bz2
 sudo -u sol wget --trust-server-names http://145.40.67.83/incremental-snapshot.tar.bz2
