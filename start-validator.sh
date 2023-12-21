13ee52cafee0fc5ce9de0b2d1721914c3e7ebb6a
leonardo3dp <88781157+leonardo3dp@users.noreply.github.com>
2023-12-21 14:19:07 -0300

    Update start-validator.sh

--- start-validator.sh
+++ start-validator.sh
@@ -3,7 +3,9 @@ exec solana-validator \
     --identity ~/validator-keypair.json \
     --vote-account ~/vote-account-keypair.json \
     --no-voting \
-    --ledger ~/ledger \
+    --ledger /mt/ledger/validator-ledger \
+    --accounts /mt/solana-accounts \
+    --log ~/log/solana-validator.log \
     --rpc-port 8899 \
     --gossip-port 8001 \
     --dynamic-port-range 8000-8020 \
@@ -19,7 +21,6 @@ exec solana-validator \
     --expected-genesis-hash 5eykt4UsFv8P8NJdTREpY1vzqKqZKvdpKuc147dw2N9d \
     --wal-recovery-mode skip_any_corrupted_record \
     --limit-ledger-size 200000000 \
-    --log ~/validator.log \
     --account-index program-id spl-token-owner spl-token-mint \
     --account-index-exclude-key kinXdEcpDQeHPEuQnqmUgtYykqKGVFq6CeVX5iAHJq6 \
     --account-index-exclude-key metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s \
